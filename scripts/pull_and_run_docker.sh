c95cdeadd88a85b53e10a79c9154aace5dbc9db0
Sahana Yogeesh <sahanajathan@gmail.com>
2024-12-05 12:30:34 +0530

    Update pull_and_run_docker.sh

--- scripts/pull_and_run_docker.sh
+++ scripts/pull_and_run_docker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
 # Ensure Docker is running
-service docker start
+if ! systemctl is-active --quiet docker; then
+    echo "Docker is not running, starting Docker..."
+    service docker start
+else
+    echo "Docker is already running."
+fi
 
 # Log in to Amazon ECR
 echo "Logging in to Amazon ECR..."
-$(aws ecr get-login --no-include-email --region us-east-1)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 843835551300.dkr.ecr.us-east-1.amazonaws.com
 
 # Pull the Docker image from ECR
 echo "Pulling Docker image from ECR..."
@@ -17,4 +23,4 @@ docker rm my-container || true
 
 # Run the new container
 echo "Running the Docker container..."
-docker run -d -p 80:80 --name my-container843835551300.dkr.ecr.us-east-1.amazonaws.com/lrn_demo2:latest
+docker run -d -p 80:80 --name my-container 843835551300.dkr.ecr.us-east-1.amazonaws.com/lrn_demo2:latest
